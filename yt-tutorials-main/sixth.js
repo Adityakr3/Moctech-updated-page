c1d1e7cafd1cc5141b5deea63ca16d616b778454
aditya <adityakumar14022002@gmail.com>
2023-03-01 16:22:27 +0530

    update popup window

---
+++ yt-tutorials-main/sixth.js
@@ -0,0 +1,12 @@
+document.querySelector(".close").addEventListener("click" , function(){
+    document.querySelector(".detspopup").style.display = "none";
+});
+
+window.addEventListener("load" , function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".detspopup").style.display = "block";
+        },
+        2000
+    )
+});
